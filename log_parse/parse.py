ff97460774b17dd250fe501b237b5d4f78f1a71c
kitsystemyou <kitsystemyou@gmail.com>
2021-09-28 06:59:08 +0900

    Refactor log_split

--- log_parse/parse.py
+++ log_parse/parse.py
@@ -1,7 +1,7 @@
-from types import ClassMethodDescriptorType
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 def log_split(content):
     content = content.replace("\r\n", "\n")
@@ -9,14 +9,16 @@ def log_split(content):
     content = content.split("\n")
     content = [x for x in content if x != '' and ('CCB' in x  or 'ccb' in x or 'CC' in x or 'cc' in x or 'RESB' in x or 'resb' in x)]
     content = [y.split(" ") for y in content]
-    
-    results = []
-    results = [[c[1], c[-3], c[-1]] for c in content if len(c)>=3] # キャラ名、出目、成功失敗
 
-    for i in results: # 全角スペース削除
-        i[0] = i[0].replace('\u3000', ' ')
+    for i in content: # 全角スペース削除
+        i[1] =i[1].replace('\u3000', ' ')
+        print(i)
 
-    return results
+    content = [[c[1], c[-3], c[-1]] for c in content if len(c)>=3] # キャラ名、出目、成功失敗
+    print(content)
+
+    return content
+
 
 #[[キャラ名、出目、成功失敗],…]となっているリストを渡すとlist(ヒストグラム)を返す関数
 def make_histogram(results):
@@ -33,6 +35,7 @@ def make_histogram(results):
     nplist = np.array(intdata)
     hist_data, _ = np.histogram(nplist, bins=10)
     return list(hist_data)
+
 
 def log_analyze(content):
     
